9d5ddd799a351e86957eb106793fd3ee745aeab0
nehal119 <34517705+nehal119@users.noreply.github.com>
2019-11-12 14:27:26 +0530

    Navbar improvements

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -36,10 +36,13 @@ class AppNavbar extends Component {
     const authLinks = (
       <Fragment>
         <NavItem>
+              <NavLink href="#">Dashboard</NavLink>
+        </NavItem>
+        {/* <NavItem>
           <span className='navbar-text mr-3'>
             <strong>{user ? `Welcome ${user.name}` : ''}</strong>
           </span>
-        </NavItem>
+        </NavItem> */}
         <NavItem>
           <Logout />
         </NavItem>
@@ -59,21 +62,76 @@ class AppNavbar extends Component {
 
     return (
       <div>
-        <Navbar color='dark' dark expand='sm' className='mb-5'>
-          <Container>
+        <Navbar color='light' light expand='sm' className='mb-5'>
             <NavbarBrand href='/'>BugTracker</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className='mr-auto' navbar>
+                <NavItem>
+                    <NavLink href="#">Home</NavLink>
+                </NavItem>
+                  <NavItem>
+                      <NavLink href="#">Feed</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink href="#">Projects</NavLink>
+                  </NavItem>
+              </Nav>
               <Nav className='ml-auto' navbar>
                 {isAuthenticated ? authLinks : guestLinks}
               </Nav>
             </Collapse>
-          </Container>
+         
         </Navbar>
       </div>
     );
   }
 }
+
+
+// <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//   <a class="navbar-brand" href="#">Navbar</a>
+//   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//     <span class="navbar-toggler-icon"></span>
+//   </button>
+
+//   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+//     <ul class="navbar-nav mr-auto">
+//       <li class="nav-item active">
+//         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="#">Link</a>
+//       </li>
+//       <li class="nav-item dropdown">
+//         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//           Dropdown
+//         </a>
+//         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+//           <a class="dropdown-item" href="#">Action</a>
+//           <a class="dropdown-item" href="#">Another action</a>
+//           <div class="dropdown-divider"></div>
+//           <a class="dropdown-item" href="#">Something else here</a>
+//         </div>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//       </li>
+//     </ul>
+//     <form class="form-inline my-2 my-lg-0">
+//       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+//       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//     </form>
+//   </div>
+// </nav>
+
+
+
+
+
+
+
+
 
 const mapStateToProps = state => ({
   auth: state.auth
