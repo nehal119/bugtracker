18d133a6d99cb2395506c55be43ee22d8e4a9afa
nehal119 <34517705+nehal119@users.noreply.github.com>
2019-12-10 14:22:05 +0530

    style improvements

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,10 +17,7 @@ export default class Home extends Component {
 				<Container className="text-center" style={{ marginTop: '10rem' }}>
 					<h1 className="mb-5 mt-5">Welcome to the Bugtracker!</h1>
 					<ListGroup style={{ marginTop: '12rem', lineHeight: '1.5rem' }}>
-						<ListGroupItem
-							className="mb-5"
-							style={{ backgroundColor: '#FFE6C4', padding: '3rem' }}
-						>
+						<ListGroupItem className="mb-5 p-3 primary-background">
 							<ListGroupItemHeading>WHY A TRACKER?</ListGroupItemHeading>
 							<hr
 								className="deep-purple accent-2 mb-2 mt-0 d-inline-block mx-auto"
@@ -36,12 +33,8 @@ export default class Home extends Component {
 								develop it? No worries! It's the right time to kick all those
 								BUGS out and become more productive as your day goes.
 							</ListGroupItemText>
-						</ListGroupItem>
-						<ListGroupItem
-							className="mb-5"
-							style={{ backgroundColor: '#FFE6C4', padding: '3rem' }}
-						>
-							<ListGroupItemHeading className="">
+
+							<ListGroupItemHeading className="mt-5">
 								WHAT DO I NEED TO DO?
 							</ListGroupItemHeading>
 							<hr
@@ -52,7 +45,7 @@ export default class Home extends Component {
 								Don't worry, we have already done the technical stuff. Now it's
 								time for you to sign up and use the features. It is completely
 								free and always will be. We respect your privacy and we got it
-								covered as well. Hence no worries at all. Just sign up and make
+								covered as well. Hence no worries at all. Register now and make
 								your life bug-free.
 							</ListGroupItemText>
 						</ListGroupItem>
